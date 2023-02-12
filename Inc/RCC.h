9397e5cb154ae23c795c53fcd71446db37e04997
Yunus-OZDEMIR <yunusozdemir@outlook.com.tr>
2023-02-13 00:01:21 +0300

    Update RCC.h

--- Inc/RCC.h
+++ Inc/RCC.h
@@ -266,18 +266,18 @@ typedef union {
 /*
  * GPIO PERIPHERAL CLOCK ENABLING OR DISABLING
  */
-#define  GPIOA_CLK_EN	 RCC->AHB1ENR |=  (1<<0)
-#define  GPIOA_CLK_DIS	 RCC->AHB1ENR &= ~(1<<0)
-#define  GPIOB_CLK_EN	 RCC->AHB1ENR |=  (1<<1)
-#define  GPIOB_CLK_DIS	 RCC->AHB1ENR &= ~(1<<1)
-#define  GPIOC_CLK_EN	 RCC->AHB1ENR |=  (1<<2)
-#define  GPIOC_CLK_DIS	 RCC->AHB1ENR &= ~(1<<2)
-#define  GPIOD_CLK_EN	 RCC->AHB1ENR |=  (1<<3)
-#define  GPIOD_CLK_DIS	 RCC->AHB1ENR &= ~(1<<3)
-#define  GPIOE_CLK_EN	 RCC->AHB1ENR |=  (1<<4)
-#define  GPIOE_CLK_DIS	 RCC->AHB1ENR &= ~(1<<4)
-#define  GPIOH_CLK_EN	 RCC->AHB1ENR |=  (1<<7)
-#define  GPIOH_CLK_DIS	 RCC->AHB1ENR &= ~(1<<7)
+#define  GPIOA_CLK_EN()	 	 RCC->AHB1ENR |=  (1<<0)
+#define  GPIOA_CLK_DIS()	 RCC->AHB1ENR &= ~(1<<0)
+#define  GPIOB_CLK_EN()	 	 RCC->AHB1ENR |=  (1<<1)
+#define  GPIOB_CLK_DIS()	 RCC->AHB1ENR &= ~(1<<1)
+#define  GPIOC_CLK_EN()	 	 RCC->AHB1ENR |=  (1<<2)
+#define  GPIOC_CLK_DIS()	 RCC->AHB1ENR &= ~(1<<2)
+#define  GPIOD_CLK_EN()	 	 RCC->AHB1ENR |=  (1<<3)
+#define  GPIOD_CLK_DIS()	 RCC->AHB1ENR &= ~(1<<3)
+#define  GPIOE_CLK_EN()	 	 RCC->AHB1ENR |=  (1<<4)
+#define  GPIOE_CLK_DIS()	 RCC->AHB1ENR &= ~(1<<4)
+#define  GPIOH_CLK_EN()	 	 RCC->AHB1ENR |=  (1<<7)
+#define  GPIOH_CLK_DIS()	 RCC->AHB1ENR &= ~(1<<7)
 
 
 #endif /* RCC_INC_RCC_H_ */
